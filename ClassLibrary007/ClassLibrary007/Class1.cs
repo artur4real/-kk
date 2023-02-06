b5b295b075dcc7c299bddc0a73456a1ffe5fb107
Artur <97942839+artur4real@users.noreply.github.com>
2023-02-06 11:22:09 +0300

    Add files via upload

---
+++ ClassLibrary007/ClassLibrary007/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zamok1
+{
+    public class Class1
+    {
+        string castlename; //название замка
+        int currentlevelHP; // текущее здоровье 
+        int max_damage; // максимальный уровень 
+        int max_level_HP; // максимальный уровень здоровья
+
+        public int СurrentlevelHP
+        {
+            get { return currentlevelHP; } // возвращает значение 
+            set { currentlevelHP = value; } // задает значение 
+
+        }
+
+        public int Max_damage
+        {
+            get { return max_damage; } // возвращает значение 
+            set { max_damage = value; } // задает значение 
+
+        }
+        public int Max_level_HP
+        {
+            get { return max_level_HP; } // возвращает значение 
+            set { max_level_HP = value; } // задает значение 
+
+
+
+        }
+    }
+}
